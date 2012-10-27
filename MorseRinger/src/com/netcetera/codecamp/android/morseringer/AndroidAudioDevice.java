23dc05f31b7a7c8d45bee9a04d639d654860c94c
pitschna <pitschna@gmail.com>
2012-10-27 09:40:14 +0200

    still trying to push...

--- MorseRinger/src/com/netcetera/codecamp/android/morseringer/AndroidAudioDevice.java
+++ MorseRinger/src/com/netcetera/codecamp/android/morseringer/AndroidAudioDevice.java
@@ -3,41 +3,73 @@ package com.netcetera.codecamp.android.morseringer;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.util.Log;
 
 public class AndroidAudioDevice {
-	AudioTrack track;
-	short[] buffer = new short[1024];
-	private int samplesWritten;
 
-	public AndroidAudioDevice(int samplingrate) {
-		int minSize = AudioTrack.getMinBufferSize(samplingrate,
-				AudioFormat.CHANNEL_CONFIGURATION_MONO,
-				AudioFormat.ENCODING_PCM_16BIT);
-		track = new AudioTrack(AudioManager.STREAM_RING, samplingrate,
-				AudioFormat.CHANNEL_CONFIGURATION_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, minSize, AudioTrack.MODE_STREAM);
-		track.play();
-	}
+  private static final String LOG_ID = "com.netcetera.codecamp.android.morseringer.AndroidAudioDevice";
+  private AudioTrack track;
+  short[] buffer = new short[1024];
+  private int samplesWritten;
+  private Boolean active = false;
 
-	public void writeSamples(float[] samples) {
-		fillBuffer(samples);
-		track.write(buffer, 0, samples.length);
-	}
+  public AndroidAudioDevice(int samplingrate) {
+    int minSize = AudioTrack.getMinBufferSize(samplingrate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+        AudioFormat.ENCODING_PCM_16BIT);
+    track = new AudioTrack(AudioManager.STREAM_RING, samplingrate,
+        AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize,
+        AudioTrack.MODE_STREAM);
+    track.play();
+    this.active = true;
+  }
 
-	public void writeSamples(short[] samples) {
-		samplesWritten = 0;
-		while (samplesWritten < samples.length) {
-			samplesWritten += track.write(samples, samplesWritten,
-					Math.min(buffer.length, samples.length - samplesWritten));
-		}
-	}
+  public Boolean writeSamples(float[] samples) {
+    Log.d(LOG_ID, "writeSamples");
+    Log.d(LOG_ID, "TRACK STATE: " + track.getState());
+    Log.d(LOG_ID, "TRACK STATE: " + track.getPlayState());
+    if (active) {
+      fillBuffer(samples);
+      track.write(buffer, 0, samples.length);
+    }
+    return active;
+  }
 
-	private void fillBuffer(float[] samples) {
-		if (buffer.length < samples.length)
-			buffer = new short[samples.length];
+  public Boolean writeSamples(short[] samples) {
+    Log.d(LOG_ID, "writeSamples");
+    Log.d(LOG_ID, "TRACK STATE: " + track.getState());
+    Log.d(LOG_ID, "TRACK STATE: " + track.getPlayState());
+    if (active) {
+      samplesWritten = 0;
+      while (samplesWritten < samples.length) {
+        samplesWritten += track.write(samples, samplesWritten,
+            Math.min(buffer.length, samples.length - samplesWritten));
+      }
+    }
+    return active;
+  }
 
-		for (int i = 0; i < samples.length; i++)
-			buffer[i] = (short) (samples[i] * Short.MAX_VALUE);
-		;
-	}
+  private void fillBuffer(float[] samples) {
+    Log.d(LOG_ID, "fillBuffer");
+    if (buffer.length < samples.length)
+      buffer = new short[samples.length];
+
+    for (int i = 0; i < samples.length; i++)
+      buffer[i] = (short) (samples[i] * Short.MAX_VALUE);;
+  }
+
+  public void stop() {
+    Log.d(LOG_ID, "stop()");
+    track.stop();
+    active = false;
+  }
+
+  public void release() {
+    Log.d(LOG_ID, "release()");
+    active = false;
+    track.release();
+  }
+
+  public Boolean isActive() {
+    return active;
+  }
 }
--- MorseRinger/src/com/netcetera/codecamp/android/morseringer/MorseSoundGenerator.java
+++ MorseRinger/src/com/netcetera/codecamp/android/morseringer/MorseSoundGenerator.java
@@ -1,153 +1,182 @@
 package com.netcetera.codecamp.android.morseringer;
 
+import android.util.Log;
+
 
 public class MorseSoundGenerator {
-	private int samprate;
-	private short[] ditBuffer = null;
-	private short[] dahBuffer = null;
-	private short[] ditPauseBuffer = null;
-	private short[] charPauseBuffer = null;
-	private short[] wordPauseBuffer = null;
-	private int DITLENGTH = 60;
-	private int DAHLENGTH = DITLENGTH * 3;
-	private int DITPAUSELENGTH = DITLENGTH;
-	private int CHARPAUSELENGTH = DAHLENGTH;
-	private int WORDPAUSELENGTH = DITLENGTH * 4;
-	private int samplesPerMillisecond;
-	private AndroidAudioDevice audioDevice;
-	private MorseCode code = new MorseCode();
-	private char[] ds;
-	private double frequency;
 
-	public MorseSoundGenerator(int samplingRate, double frequency, int ditLength) {
-		this.samprate = samplingRate;
-		this.frequency = frequency;
-		this.samplesPerMillisecond = samprate / 1000;
-		this.DITLENGTH = ditLength;
-		this.DAHLENGTH = DITLENGTH * 3;
-		this.DITPAUSELENGTH = DITLENGTH;
-		this.CHARPAUSELENGTH = DITLENGTH * 4;
-		this.WORDPAUSELENGTH = DITLENGTH * 7;
-		ditBuffer = new short[getBufferSize(DITLENGTH)];
-		dahBuffer = new short[getBufferSize(DAHLENGTH)];
-		ditPauseBuffer = new short[getBufferSize(DITPAUSELENGTH)];
-		charPauseBuffer = new short[getBufferSize(CHARPAUSELENGTH)];
-		wordPauseBuffer = new short[getBufferSize(WORDPAUSELENGTH)];
-		fillBuffer(ditBuffer, false);
-		fillBuffer(dahBuffer, false);
-		fillBuffer(ditPauseBuffer, true);
-		fillBuffer(charPauseBuffer, true);
-		fillBuffer(wordPauseBuffer, true);
-		deGlitchBuffer(ditBuffer);
-		deGlitchBuffer(dahBuffer);
-	}
+  private static final String LOG_ID = "com.netcetera.codecamp.android.morseringer.MorseSoundGenerator";
+  private int samprate;
+  private short[] ditBuffer = null;
+  private short[] dahBuffer = null;
+  private short[] ditPauseBuffer = null;
+  private short[] charPauseBuffer = null;
+  private short[] wordPauseBuffer = null;
+  private int DITLENGTH = 60;
+  private int DAHLENGTH = DITLENGTH * 3;
+  private int DITPAUSELENGTH = DITLENGTH;
+  private int CHARPAUSELENGTH = DAHLENGTH;
+  private int WORDPAUSELENGTH = DITLENGTH * 4;
+  private int samplesPerMillisecond;
+  private AndroidAudioDevice audioDevice;
+  private MorseCode code = new MorseCode();
+  private char[] ds;
+  private double frequency;
+  private Thread morserThread;
 
-	private void deGlitchBuffer(short[] buffer) {
-		int fadeLengthMs = 2;//MUST be > 0 (Div / 0)
-		int fadeLengthSamples = fadeLengthMs * samplesPerMillisecond;
-		if (buffer.length > fadeLengthSamples * 3) {
-			//Only fade in/out if buffer is long enough
-			short toValIn = buffer[fadeLengthSamples];
-			float stepIn = (float) toValIn / (float) fadeLengthSamples;
-			short fromValOut = buffer[buffer.length - fadeLengthSamples];
-			float stepOut = (float) fromValOut / (float) fadeLengthSamples;
-			//Fade-IN
-			for (int i = 0; i < fadeLengthSamples; i++) {
-				buffer[i] = (short) (stepIn * i);
-			}
-			//Fade-OUT
-			for (int i = 1; i <= fadeLengthSamples; i++) {
-				buffer[buffer.length - i] =
-				    (short) (stepOut * (i - 1));
-			}
-		}
-	}
+  public MorseSoundGenerator(int samplingRate, double frequency, int ditLength) {
+    Log.d(LOG_ID, "MorseSoundGenerator");
+    this.samprate = samplingRate;
+    this.frequency = frequency;
+    this.samplesPerMillisecond = samprate / 1000;
+    this.DITLENGTH = ditLength;
+    this.DAHLENGTH = DITLENGTH * 3;
+    this.DITPAUSELENGTH = DITLENGTH;
+    this.CHARPAUSELENGTH = DITLENGTH * 4;
+    this.WORDPAUSELENGTH = DITLENGTH * 7;
+    ditBuffer = new short[getBufferSize(DITLENGTH)];
+    dahBuffer = new short[getBufferSize(DAHLENGTH)];
+    ditPauseBuffer = new short[getBufferSize(DITPAUSELENGTH)];
+    charPauseBuffer = new short[getBufferSize(CHARPAUSELENGTH)];
+    wordPauseBuffer = new short[getBufferSize(WORDPAUSELENGTH)];
+    fillBuffer(ditBuffer, false);
+    fillBuffer(dahBuffer, false);
+    fillBuffer(ditPauseBuffer, true);
+    fillBuffer(charPauseBuffer, true);
+    fillBuffer(wordPauseBuffer, true);
+    deGlitchBuffer(ditBuffer);
+    deGlitchBuffer(dahBuffer);
+  }
 
-	private void writeCharacterToAudioBuffer(char c) {
-		ds = code.getMorseCode(c);
-		if (null == ds) {
-			audioDevice.writeSamples(getWordPauseBuffer());
-			return;
-		}
-		for (int i = 0; i < ds.length; i++) {
-			if (ds[i] == '.') {
-				audioDevice.writeSamples(getDitBuffer());
-			} else if (ds[i] == '-') {
-				audioDevice.writeSamples(getDahBuffer());
-			} else {
-				audioDevice.writeSamples(getWordPauseBuffer());
-			}
-			if (i == ds.length - 1) {//add character pause
-				audioDevice.writeSamples(getCharPauseBuffer());
-			} else {//add dit-pause
-				audioDevice.writeSamples(getDitPauseBuffer());
-			}
-		}
-	}
+  private void deGlitchBuffer(short[] buffer) {
+    Log.d(LOG_ID, "deGlitchBuffer");
+    int fadeLengthMs = 2;// MUST be > 0 (Div / 0)
+    int fadeLengthSamples = fadeLengthMs * samplesPerMillisecond;
+    if (buffer.length > fadeLengthSamples * 3) {
+      // Only fade in/out if buffer is long enough
+      short toValIn = buffer[fadeLengthSamples];
+      float stepIn = (float) toValIn / (float) fadeLengthSamples;
+      short fromValOut = buffer[buffer.length - fadeLengthSamples];
+      float stepOut = (float) fromValOut / (float) fadeLengthSamples;
+      // Fade-IN
+      for (int i = 0; i < fadeLengthSamples; i++) {
+        buffer[i] = (short) (stepIn * i);
+      }
+      // Fade-OUT
+      for (int i = 1; i <= fadeLengthSamples; i++) {
+        buffer[buffer.length - i] = (short) (stepOut * (i - 1));
+      }
+    }
+  }
 
-	private int getBufferSize(int length) {
-		return samplesPerMillisecond * length;
-	}
+  private void writeCharacterToAudioBuffer(char c) {
+    Log.d(LOG_ID, "writeCharacterToAudioBuffer");
+    ds = code.getMorseCode(c);
+    if (null == ds && audioDevice.isActive()) {
+      audioDevice.writeSamples(getWordPauseBuffer());
+      return;
+    }
+    for (int i = 0; i < ds.length && audioDevice.isActive(); i++) {
+      if (ds[i] == '.') {
+        audioDevice.writeSamples(getDitBuffer());
+      } else if (ds[i] == '-') {
+        audioDevice.writeSamples(getDahBuffer());
+      } else {
+        audioDevice.writeSamples(getWordPauseBuffer());
+      }
+      if (i == ds.length - 1) {// add character pause
+        audioDevice.writeSamples(getCharPauseBuffer());
+      } else {// add dit-pause
+        audioDevice.writeSamples(getDitPauseBuffer());
+      }
+    }
+  }
 
-	void fillBuffer(short[] buffer, boolean pause) {
-		double omega, t;
-		double dt = 1.0 / samprate; // sec per samp
+  private int getBufferSize(int length) {
+    Log.d(LOG_ID, "getBufferSize");
+    return samplesPerMillisecond * length;
+  }
 
-		if (pause) {
-			for (int i = 0; i < buffer.length; i++) {
-				buffer[i] = (short) 0;
-			}
-		} else {
-			t = 0.0;
-			omega = (float) (2.0 * Math.PI * frequency);
-			for (int i = 0; i < buffer.length; i++) {
-				buffer[i] = (short) (Short.MAX_VALUE * Math.sin(omega * t));
-				t += dt;
-			}
-		}
-	}
+  void fillBuffer(short[] buffer, boolean pause) {
+    Log.d(LOG_ID, "fillBuffer");
+    double omega, t;
+    double dt = 1.0 / samprate; // sec per samp
 
-	public short[] getBuffer(int lengthMs, boolean mute) {
-		short [] buffer = new short[samplesPerMillisecond * lengthMs];
-		fillBuffer(buffer, mute);
-		return buffer;
-	}
+    if (pause) {
+      for (int i = 0; i < buffer.length; i++) {
+        buffer[i] = (short) 0;
+      }
+    } else {
+      t = 0.0;
+      omega = (float) (2.0 * Math.PI * frequency);
+      for (int i = 0; i < buffer.length; i++) {
+        buffer[i] = (short) (Short.MAX_VALUE * Math.sin(omega * t));
+        t += dt;
+      }
+    }
+  }
 
-	public short[] getDitBuffer() {
-		return ditBuffer;
-	}
+  public short[] getBuffer(int lengthMs, boolean mute) {
+    Log.d(LOG_ID, "getBuffer");
+    short[] buffer = new short[samplesPerMillisecond * lengthMs];
+    fillBuffer(buffer, mute);
+    return buffer;
+  }
 
-	public short[] getDahBuffer() {
-		return dahBuffer;
-	}
+  public short[] getDitBuffer() {
+    Log.d(LOG_ID, "getDitBuffer");
+    return ditBuffer;
+  }
 
-	public short[] getDitPauseBuffer() {
-		return ditPauseBuffer;
-	}
+  public short[] getDahBuffer() {
+    Log.d(LOG_ID, "getDahBuffer");
+    return dahBuffer;
+  }
 
-	public short[] getCharPauseBuffer() {
-		return charPauseBuffer;
-	}
+  public short[] getDitPauseBuffer() {
+    Log.d(LOG_ID, "getDitPauseBuffer");
+    return ditPauseBuffer;
+  }
 
-	public short[] getWordPauseBuffer() {
-		return wordPauseBuffer;
-	}
+  public short[] getCharPauseBuffer() {
+    Log.d(LOG_ID, "getCharPauseBuffer");
+    return charPauseBuffer;
+  }
 
-	public void morse(final String string) {
-		audioDevice = new AndroidAudioDevice(samprate);
-		char[] charArray = string.toUpperCase().toCharArray();
-		for (int i = 0; i < charArray.length; i++) {
-			writeCharacterToAudioBuffer(charArray[i]);
-		}
-	}
+  public short[] getWordPauseBuffer() {
+    Log.d(LOG_ID, "getWordPauseBuffer");
+    return wordPauseBuffer;
+  }
 
-	public void stop() {
-		audioDevice.track.stop();
-	}
+  public void morse(final String string) {
+    Log.d(LOG_ID, "morse - preparing Morsestuff");
+    audioDevice = new AndroidAudioDevice(samprate);
+    final char[] charArray = string.toUpperCase().toCharArray();
+    morserThread = new Thread() {
 
-	public void release() {
-		audioDevice.track.release();
-	}
+      @Override
+      public void run() {
+        super.run();
+        for (int i = 0; i < charArray.length && audioDevice.isActive(); i++) {
+          writeCharacterToAudioBuffer(charArray[i]);
+        }
+      }
+
+    };
+    Log.d(LOG_ID, "morse - about to kick off Morser thread");
+    morserThread.start();
+    Log.d(LOG_ID, "morse started - returning.");
+  }
+
+  public void stop() {
+    Log.d(LOG_ID, "stop");
+    audioDevice.stop();
+  }
+
+  public void release() {
+    Log.d(LOG_ID, "release");
+    audioDevice.release();
+  }
 
 
 }
