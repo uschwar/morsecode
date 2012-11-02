d6ed69d47b42b11b47a57bca69e99c2ec8511537
Ulrich Schwar <ulrich.schwar@gmail.com>
2012-11-02 21:49:23 +0100

    work in progress on storing preferences

--- MorseRinger/src/at/schwar/android/morse/gui/MainScreen.java
+++ MorseRinger/src/at/schwar/android/morse/gui/MainScreen.java
@@ -1,9 +1,10 @@
 package at.schwar.android.morse.gui;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
@@ -11,102 +12,114 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import at.schwar.android.morse.service.MorseService;
 
 public class MainScreen extends Activity implements OnGestureListener {
 
-  protected Bundle savedInstanceState;
-  private Context context;
+	protected Bundle savedInstanceState;
+	private Context context;
 
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    context = this;
-    this.savedInstanceState = savedInstanceState;
-    setContentView(R.layout.activity_main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		context = this;
+		this.savedInstanceState = savedInstanceState;
+		setContentView(R.layout.activity_main);
 
-    addListenerOnCheckBoxEnable();
-    addListenerOnClickShowMorseCode();
+		addListenerOnCheckBoxEnable();
+		addListenerOnClickShowMorseCode();
 
-  }
+		loadPreferences();
 
+	}
 
-  private void addListenerOnClickShowMorseCode() {
-    Button showMorseCode = (Button) findViewById(R.id.button_show_morsecode);
-    showMorseCode.setOnClickListener(new OnClickListener() {
+	private void loadPreferences() {
+		SharedPreferences preferences = context.getSharedPreferences(
+				MorseService.class.getCanonicalName(), Context.MODE_PRIVATE);
+		CheckBox checkBoxEnable = (CheckBox) findViewById(R.id.checkbox_enable);
+		checkBoxEnable.setChecked(preferences.getBoolean(MorseService.ENABLED_NAME, true));
+		
+	}
 
-      public void onClick(View v) {
-        startActivity(new Intent(context,ListViewActivity.class)); 
-      }
-    });
-  }
+	private void addListenerOnClickShowMorseCode() {
+		Button showMorseCode = (Button) findViewById(R.id.button_show_morsecode);
+		showMorseCode.setOnClickListener(new OnClickListener() {
 
+			public void onClick(View v) {
+				startActivity(new Intent(context, ListViewActivity.class));
+			}
+		});
+	}
 
-  private void addListenerOnCheckBoxEnable() {
-    CheckBox checkBoxEnable = (CheckBox) findViewById(R.id.checkbox_enable);
-    checkBoxEnable.setOnClickListener(new OnClickListener() {
+	private void addListenerOnCheckBoxEnable() {
+		final CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox_enable);
+		checkBox.setOnClickListener(new OnClickListener() {
 
-      public void onClick(View v) {
-        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox_enable);
-        if (!checkBox.isChecked()) {
-          checkBox.setChecked(true);
-        }
-      }
-    });
-  }
+			public void onClick(View v) {
+				SharedPreferences preferences = context.getSharedPreferences(
+						MorseService.class.getCanonicalName(),
+						Context.MODE_PRIVATE);
+				Editor editor = preferences.edit();
+				editor.putBoolean(MorseService.ENABLED_NAME,
+						checkBox.isChecked());
+				editor.apply();
+			}
+		});
+	}
 
+	// ----------lifecycle callbacks----------------
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
 
-  // ----------lifecycle callbacks----------------
-  @Override
-  public void onPause() {
-    super.onPause();
-  }
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
 
-  @Override
-  public void onResume() {
-    super.onResume();
-  }
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
 
-  @Override
-  public void onStop() {
-    super.onStop();
-  }
+	// ------------gesture callbacks--------------------
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		return true;
+	}
 
-  // ------------gesture callbacks--------------------
-  @Override
-  public boolean onTouchEvent(MotionEvent e) {
-    return true;
-  }
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-  public boolean onDown(MotionEvent e) {
-    // TODO Auto-generated method stub
-    return false;
-  }
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-    // TODO Auto-generated method stub
-    return false;
-  }
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
 
-  public void onLongPress(MotionEvent e) {
-    // TODO Auto-generated method stub
+	}
 
-  }
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-  public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-    // TODO Auto-generated method stub
-    return false;
-  }
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
 
-  public void onShowPress(MotionEvent e) {
-    // TODO Auto-generated method stub
+	}
 
-  }
-
-  public boolean onSingleTapUp(MotionEvent e) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- MorseService/src/at/schwar/android/morse/service/MorseService.java
+++ MorseService/src/at/schwar/android/morse/service/MorseService.java
@@ -1,6 +1,5 @@
 package at.schwar.android.morse.service;
 
-
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.IBinder;
@@ -12,144 +11,159 @@ import at.schwar.android.morse.service.ext.NewContactsHelper;
 
 public class MorseService extends IntentService {
 
-  private static final String LOG_ID = "com.netcetera.codecamp.android.morseringer.MorseService";
-  private static MorseSoundGenerator morseSoundGenerator = null;
-  private ContactsHelper contactsHelper = new NewContactsHelper();
-  private static final int SAMPLINGRATE = 44100;
+	public static final String ENABLED_NAME = "enabled";
 
-  public MorseService() {
-    super("noname");
-    Log.d(LOG_ID, "MorseService()");
-    // TODO Auto-generated constructor stub
-  }
-  
-  public MorseService(String name) {
-    super(name);
-    Log.d(LOG_ID, "MorseService(String name)");
-  }
+	private static final String LOG_ID = MorseService.class.getCanonicalName();
+	private static MorseSoundGenerator morseSoundGenerator = null;
+	private ContactsHelper contactsHelper = new NewContactsHelper();
+	private static final int SAMPLINGRATE = 44100;
 
-  @Override
-  public void onCreate() {
-    // TODO Auto-generated method stub
-    super.onCreate();
-    Log.d(LOG_ID, "onCreate()");
-  }
+	public MorseService() {
+		super("noname");
+		Log.d(LOG_ID, "MorseService()");
+		// TODO Auto-generated constructor stub
+	}
 
-  @Override
-  public void onDestroy() {
-    // TODO Auto-generated method stub
-    super.onDestroy();
-    Log.d(LOG_ID, "onDestroy()");
-  }
+	public MorseService(String name) {
+		super(name);
+		Log.d(LOG_ID, "MorseService(String name)");
+	}
 
-  @Override
-  public void onStart(Intent intent, int startId) {
-    // TODO Auto-generated method stub
-    super.onStart(intent, startId);
-    handleCommand(intent, startId);
-    Log.d(LOG_ID, "onStart()");
-  }
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Log.d(LOG_ID, "onCreate()");
+	}
 
-  @Override
-  public IBinder onBind(Intent intent) {
-    // TODO Auto-generated method stub
-    Log.d(LOG_ID, "onBind");
-    return null;
-  }
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d(LOG_ID, "onDestroy()");
+	}
 
-  @Override
-  protected void onHandleIntent(Intent arg0) {
-    // TODO Auto-generated method stub
-    Log.d(LOG_ID, "onHandleIntent");
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+		handleCommand(intent, startId);
+		Log.d(LOG_ID, "onStart()");
+	}
 
-  }
-  
-  
-  protected void handleCommand(Intent intent, int startId){
-    final String phoneState = intent
-                    .getStringExtra(TelephonyManager.EXTRA_STATE);
-    final String phoneNumber = intent
-                    .getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		Log.d(LOG_ID, "onBind");
+		return null;
+	}
 
-    if (TelephonyManager.EXTRA_STATE_RINGING.equals(phoneState)) {
-      CallerIDResult result = contactsHelper .getContact(phoneNumber, this.getContentResolver());
-      Log.d(LOG_ID, "Lookup result is " + result.getName());
-       if (!"".equals(result.getName())) {
-        getMorser().morseForever(result.getName());
-      }
-    } else {//QUIET! NOW!
-      stopMorser();
-    }
-    
-//    // since we're about to start a new lookup or the phone stopped ringing,
-//    // we want to cancel any lookups in progress
-//    if (currentLookupAsyncTask != null)
-//            currentLookupAsyncTask.cancel(true);
-//
-//    if (TelephonyManager.EXTRA_STATE_RINGING.equals(phoneState)) {
-//            if(TextUtils.isEmpty(phoneNumber) || SpecialPhoneNumbers.UNKNOWN_NUMBER.equals(phoneNumber)){
-//                    toastLayout.setVisibility(View.GONE);
-//                    textToSpeechHelper.speak(getString(R.string.incoming_call_tts_unknown), TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
-//            }else if(SpecialPhoneNumbers.PRIVATE_NUMBER.equals(phoneNumber)){
-//                    toastLayout.setVisibility(View.GONE);
-//                    textToSpeechHelper.speak(getString(R.string.incoming_call_tts_private), TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
-//            }else if(SpecialPhoneNumbers.PAYPHONE_NUMBER.equals(phoneNumber)){
-//                    toastLayout.setVisibility(View.GONE);
-//                    textToSpeechHelper.speak(getString(R.string.incoming_call_tts_payphone), TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
-//            }else{
-//                    try{
-//                            CallerIDResult result = contactsHelper.getContact(phoneNumber);
-//                            toastLayout.setVisibility(View.GONE);
-//                            //speak the contact's name even when we don't need to use the CallerID service to get information
-//                            
-//                            if(ttsEnabled && result.getName()!=null && result.getName()!=""){
-//                                    textToSpeechHelper.speak(getString(R.string.incoming_call_tts, result.getName()), TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
-//                            }
-//                    }catch(NoResultException e){
-//                            currentLookupAsyncTask = new ToastLookupAsyncTask(this, phoneNumber);
-//                            currentLookupAsyncTask.execute();
-//                    }
-//            }
-//    } else {
-//            toastLayout.setVisibility(View.GONE);
-//            stopSelf(startId);
-//    }
-//    
-//    if (TelephonyManager.EXTRA_STATE_IDLE.equals(phoneState)
-//                    && TelephonyManager.EXTRA_STATE_RINGING.equals(previousPhoneState)
-//                    && previousPhoneNumber!=null
-//                    && !contactsHelper.haveContactWithPhoneNumber(previousPhoneNumber)){
-//            //add missed call notification
-//            final Notification notification = new Notification(
-//                            android.R.drawable.sym_call_missed,
-//                            previousCallerID==null?getString(R.string.missed_call_from_unknown):MessageFormat.format(getString(R.string.missed_call_from_known),previousCallerID),
-//                            System.currentTimeMillis());
-//            final Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-//            notificationIntent.putExtra("phoneNumber", previousPhoneNumber);
-//            final PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-//            notification.setLatestEventInfo(getApplicationContext(), getString(R.string.missed_call), previousCallerID==null?getString(R.string.perform_lookup_label):previousCallerID, contentIntent);
-//            notification.flags |= Notification.FLAG_AUTO_CANCEL;
-//            notificationManager.notify(new Random().nextInt(), notification);
-//            
-//            if(ttsEnabled){
-//                    textToSpeechHelper.stop();
-//            }
-//    }
-//    previousPhoneNumber = phoneNumber;
-//    previousPhoneState = phoneState;
-}
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+		Log.d(LOG_ID, "onHandleIntent");
 
-  private void stopMorser() {
-    if (null != morseSoundGenerator) {
-      morseSoundGenerator.stop();
-      morseSoundGenerator.release();
-    }
-  }
+	}
 
-  private MorseSoundGenerator getMorser() {
-    if (null == morseSoundGenerator) {
-      morseSoundGenerator  = new MorseSoundGenerator(SAMPLINGRATE, 800.0, 50);
-    }
-    return morseSoundGenerator;
-  }
+	protected void handleCommand(Intent intent, int startId) {
+		final String phoneState = intent
+				.getStringExtra(TelephonyManager.EXTRA_STATE);
+		final String phoneNumber = intent
+				.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+		if (TelephonyManager.EXTRA_STATE_RINGING.equals(phoneState)) {
+			CallerIDResult result = contactsHelper.getContact(phoneNumber,
+					this.getContentResolver());
+			Log.d(LOG_ID, "Lookup result is " + result.getName());
+			if (!"".equals(result.getName())) {
+				getMorser().morseForever(result.getName());
+			}
+		} else {// QUIET! NOW!
+			stopMorser();
+		}
+
+		// // since we're about to start a new lookup or the phone stopped
+		// ringing,
+		// // we want to cancel any lookups in progress
+		// if (currentLookupAsyncTask != null)
+		// currentLookupAsyncTask.cancel(true);
+		//
+		// if (TelephonyManager.EXTRA_STATE_RINGING.equals(phoneState)) {
+		// if(TextUtils.isEmpty(phoneNumber) ||
+		// SpecialPhoneNumbers.UNKNOWN_NUMBER.equals(phoneNumber)){
+		// toastLayout.setVisibility(View.GONE);
+		// textToSpeechHelper.speak(getString(R.string.incoming_call_tts_unknown),
+		// TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
+		// }else if(SpecialPhoneNumbers.PRIVATE_NUMBER.equals(phoneNumber)){
+		// toastLayout.setVisibility(View.GONE);
+		// textToSpeechHelper.speak(getString(R.string.incoming_call_tts_private),
+		// TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
+		// }else if(SpecialPhoneNumbers.PAYPHONE_NUMBER.equals(phoneNumber)){
+		// toastLayout.setVisibility(View.GONE);
+		// textToSpeechHelper.speak(getString(R.string.incoming_call_tts_payphone),
+		// TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
+		// }else{
+		// try{
+		// CallerIDResult result = contactsHelper.getContact(phoneNumber);
+		// toastLayout.setVisibility(View.GONE);
+		// //speak the contact's name even when we don't need to use the
+		// CallerID service to get information
+		//
+		// if(ttsEnabled && result.getName()!=null && result.getName()!=""){
+		// textToSpeechHelper.speak(getString(R.string.incoming_call_tts,
+		// result.getName()), TextToSpeech.QUEUE_FLUSH, ttsParametersMap);
+		// }
+		// }catch(NoResultException e){
+		// currentLookupAsyncTask = new ToastLookupAsyncTask(this, phoneNumber);
+		// currentLookupAsyncTask.execute();
+		// }
+		// }
+		// } else {
+		// toastLayout.setVisibility(View.GONE);
+		// stopSelf(startId);
+		// }
+		//
+		// if (TelephonyManager.EXTRA_STATE_IDLE.equals(phoneState)
+		// && TelephonyManager.EXTRA_STATE_RINGING.equals(previousPhoneState)
+		// && previousPhoneNumber!=null
+		// && !contactsHelper.haveContactWithPhoneNumber(previousPhoneNumber)){
+		// //add missed call notification
+		// final Notification notification = new Notification(
+		// android.R.drawable.sym_call_missed,
+		// previousCallerID==null?getString(R.string.missed_call_from_unknown):MessageFormat.format(getString(R.string.missed_call_from_known),previousCallerID),
+		// System.currentTimeMillis());
+		// final Intent notificationIntent = new Intent(getApplicationContext(),
+		// MainActivity.class);
+		// notificationIntent.putExtra("phoneNumber", previousPhoneNumber);
+		// final PendingIntent contentIntent = PendingIntent.getActivity(this,
+		// 0, notificationIntent, 0);
+		// notification.setLatestEventInfo(getApplicationContext(),
+		// getString(R.string.missed_call),
+		// previousCallerID==null?getString(R.string.perform_lookup_label):previousCallerID,
+		// contentIntent);
+		// notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		// notificationManager.notify(new Random().nextInt(), notification);
+		//
+		// if(ttsEnabled){
+		// textToSpeechHelper.stop();
+		// }
+		// }
+		// previousPhoneNumber = phoneNumber;
+		// previousPhoneState = phoneState;
+	}
+
+	private void stopMorser() {
+		if (null != morseSoundGenerator) {
+			morseSoundGenerator.stop();
+			morseSoundGenerator.release();
+		}
+	}
+
+	private MorseSoundGenerator getMorser() {
+		if (null == morseSoundGenerator) {
+			morseSoundGenerator = new MorseSoundGenerator(SAMPLINGRATE, 800.0,
+					50);
+		}
+		return morseSoundGenerator;
+	}
 }
